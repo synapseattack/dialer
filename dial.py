1e5125b93c0eafd9116819687e75d59773b4ec22
Ronald Franks <rfranks@twilio.com>
2016-07-18 17:17:41 -0700

    fix some shit

--- dial.py
+++ dial.py
@@ -1,16 +1,19 @@
-import twilio
+from twilio import twiml
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    dialednumber = request.values.get('to')
-    resp = twilio.twiml.Response()
+    dialednumber = str(request.values.get('To'))
+    resp = twiml.Response()
     resp.dial(dialednumber)
+    # resp.dial(dialednumber)
     return str(resp)
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
